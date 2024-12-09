cbf87fd4d2d6bf701309462030a98f4916a15956
Dhanusha <dhanusham583@gmail.com>
2024-12-09 12:03:11 +0530

    data_validation added

--- signLanguage/pipeline/training_pipeline.py
+++ signLanguage/pipeline/training_pipeline.py
@@ -2,20 +2,20 @@ import sys, os
 from signLanguage.logger import logging
 from signLanguage.exception import SignException
 from signLanguage.components.data_ingestion import DataIngestion
-# from signLanguage.components.data_validation import DataValidation
+from signLanguage.components.data_validation import DataValidation
 # from signLanguage.components.model_trainer import ModelTrainer
 # from signLanguage.components.model_pusher import ModelPusher
 # from signLanguage.configuration.s3_operations import S3Operation
 
 
-from signLanguage.entity.config_entity import (DataIngestionConfig)
-                                            #    DataValidationConfig,
+from signLanguage.entity.config_entity import (DataIngestionConfig,
+                                               DataValidationConfig)
                                             #    ModelTrainerConfig,
                                             #    ModelPusherConfig)
 
 
-from signLanguage.entity.artifacts_entity import (DataIngestionArtifact)
-                                                #   DataValidationArtifact,
+from signLanguage.entity.artifacts_entity import (DataIngestionArtifact,
+                                                  DataValidationArtifact)
                                                 #   ModelTrainerArtifact,
                                                 #   ModelPusherArtifacts)
 
@@ -23,7 +23,7 @@ from signLanguage.entity.artifacts_entity import (DataIngestionArtifact)
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-        # self.data_validation_config = DataValidationConfig()
+        self.data_validation_config = DataValidationConfig()
         # self.model_trainer_config = ModelTrainerConfig()
         # self.model_pusher_config = ModelPusherConfig()
         # self.s3_operations = S3Operation()
@@ -54,29 +54,29 @@ class TrainPipeline:
         
 
     
-    # def start_data_validation(
-    #     self, data_ingestion_artifact: DataIngestionArtifact
-    # ) -> DataValidationArtifact:
-    #     logging.info("Entered the start_data_validation method of TrainPipeline class")
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
 
-    #     try:
-    #         data_validation = DataValidation(
-    #             data_ingestion_artifact=data_ingestion_artifact,
-    #             data_validation_config=self.data_validation_config,
-    #         )
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
 
-    #         data_validation_artifact = data_validation.initiate_data_validation()
+            data_validation_artifact = data_validation.initiate_data_validation()
 
-    #         logging.info("Performed the data validation operation")
+            logging.info("Performed the data validation operation")
 
-    #         logging.info(
-    #             "Exited the start_data_validation method of TrainPipeline class"
-    #         )
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
 
-    #         return data_validation_artifact
+            return data_validation_artifact
 
-    #     except Exception as e:
-    #         raise SignException(e, sys) from e
+        except Exception as e:
+            raise SignException(e, sys) from e
         
 
     
@@ -114,9 +114,9 @@ class TrainPipeline:
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-            # data_validation_artifact = self.start_data_validation(
-            #     data_ingestion_artifact=data_ingestion_artifact
-            # )
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
 
             # if data_validation_artifact.validation_status == True:
             #     model_trainer_artifact = self.start_model_trainer()
